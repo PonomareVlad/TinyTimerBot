476c39e7097443757c8794ca25795c0b7b85225f
Vladislav Ponomarev <2877584+PonomareVlad@users.noreply.github.com>
2023-06-20 14:32:58 +0500

    Update update.mjs

--- api/update.mjs
+++ api/update.mjs
@@ -5,7 +5,7 @@ import {webhookStream} from "vercel-grammy/edge.mjs";
 export const config = {runtime: "edge"};
 
 export default webhookStream(bot, {
-    timeoutMilliseconds: 24 * 60 * 60 * 1000,
+    timeoutMilliseconds: 16 * 60 * 1000,
     onTimeout: "return",
     secretToken,
 });
